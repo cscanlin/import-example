f3949989163abcfdd7c4e71d5958a56f6dbf6ac5
Chris Scanlin <cscanlin@gmail.com>
2017-10-16 19:16:03 -0700

    Add example and readme

---
+++ main.py
@@ -0,0 +1,4 @@
+from modules.mymod import my_func
+
+if __name__ == '__main__':
+    my_func()
---
+++ modules/mymod.py
@@ -0,0 +1,7 @@
+import modules.config as config
+
+def my_func():
+    print(config.CONF_VAR)
+
+if __name__ == '__main__':
+    my_func()
